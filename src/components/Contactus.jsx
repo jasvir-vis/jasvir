a4797388e578f2026269e58372b1e7e3c1a60df0
jasvir-vis <159865153+jasvir-vis@users.noreply.github.com>
2025-07-16 17:24:59 +0530

    Update Contactus.jsx

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
-
 import { motion } from 'framer-motion'
 
 const Contactus = () => {
   return (
-    <div className='border-b border-neutral-800 pb-24 container'>
-        <motion.h1  
-         whileInView={{opacity: 1, y: 0}}
-         initial= {{opacity:0, y:-100}}
-         transition={{duration: 0.5}}
-         className="my-20 text-center text-blue-900 text-4xl">CONTACTS</motion.h1>
+    <div className='border-b border-neutral-800 pb-24 container mx-auto px-4'>
+      <motion.h1  
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-blue-900 text-4xl"
+      >
+        CONTACTS
+      </motion.h1>
 
-        <motion.div
-         whileInView={{opacity: 1, x: 0}}
-         initial= {{opacity:0, x:100}}
-         transition={{duration: 1}}
-         className='px-0 lg:px-12'>
-            <div className="mb-4 d-flex flex-column gap-3 justify-content-center align-item-center">
-                <h3 className="text-dark text-center my-2">Address :</h3>
-                <h4 className="text-center text-muted mb-2">Attalgarh, PO - Mukerian, Distt - Hoshiarpur</h4>
-                <h4 className="text-muted text-center">Punjab (144211)</h4>
-              <br/>
-              <br/>
-              <h3 className="text-center">Mobile:</h3>
-              <h4 className="text-muted text-center" >+91 83604-19086</h4>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1 }}
+        className='px-0 lg:px-12'
+      >
+        <div className="mb-4 flex flex-col gap-4 justify-center items-center text-center">
+          <h3 className="text-dark text-xl">Address:</h3>
+          <h4 className="text-muted text-lg">Attalgarh, PO - Mukerian, Distt - Hoshiarpur</h4>
+          <h4 className="text-muted text-lg">Punjab (144211)</h4>
 
-              <h3 className="text-center">Email:</h3>
-              <h4 className="text-muted text-center">jasvishal@gmail.com</h4>
-            </div>
-        </motion.div>
+          <br />
+
+          <h3 className="text-dark text-xl">Mobile:</h3>
+          <h4 className="text-muted text-lg">+91 83604-19086</h4>
+
+          <br />
+
+          <h3 className="text-dark text-xl">Email:</h3>
+          <h4 className="text-muted text-lg">jasvishal@gmail.com</h4>
+        </div>
+      </motion.div>
     </div>
   )
 }
